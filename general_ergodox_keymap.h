5b27c76d9da04176a00927041de0befb79a7f29f
Tom Craig <thetomcraig@icloud.com>
2019-05-28 16:38:44 -0700

    Rework comments, add numpad, change modifiers

--- general_ergodox_keymap.h
+++ general_ergodox_keymap.h
@@ -1,8 +1,10 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
-#define BASE 0 // default layer
-#define NAV 1 // symbols
+#include "secrets.h"
 
+#define BASE 0  // default layer
+#define LYR1 1  // symbols
+#define LYR2 2  // numpad
 
 enum custom_keycodes {
   TMUX_N = SAFE_RANGE,
@@ -11,16 +13,64 @@ enum custom_keycodes {
   TMUX_CYC,
   PRV_WD,
   NXT_WD,
-  PASSWD,
   EPRM,
   VRSN,
-  RGB_SLD
+  RGB_SLD,
+  P_CMP,
+  P_1PASS,
+  P_OKTA,
+  P_CP,
+  P_LP,
 };
 
+
+// This is only for passwords
+// These string edfined in a file NOT in VCS
+bool process_password_keys(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case P_CMP:
+      if (record->event.pressed) {
+        SEND_STRING(computer_password);
+        SEND_STRING(SS_TAP(X_ENTER));
+      }
+      break;
+    case P_1PASS:
+      if (record->event.pressed) {
+        SEND_STRING(one_pass_password);
+        SEND_STRING(SS_TAP(X_ENTER));
+      }
+      break;
+    case P_OKTA:
+      if (record->event.pressed) {
+        SEND_STRING(okta_password);
+        SEND_STRING(SS_TAP(X_ENTER));
+      }
+      break;
+    case P_CP:
+      if (record->event.pressed) {
+        SEND_STRING(coachportal_password);
+        SEND_STRING(SS_TAP(X_ENTER));
+      }
+      break;
+    case P_LP:
+      if (record->event.pressed) {
+        SEND_STRING(last_pass_password);
+        SEND_STRING(SS_TAP(X_ENTER));
+      }
+      break;
+  }
+  return true;
+}
 
 // This is only for my custom macros
 // Called by the general "process_record_user"
 bool process_custom_macro_keys(uint16_t keycode, keyrecord_t *record) {
+  // First call helper function to process if it's a password macro
+  bool still_need_to_process = process_password_keys(keycode, record);
+  if (still_need_to_process == false) {
+    return false;
+  }
+  // Not a password macro; move on
   switch (keycode) {
     case TMUX_N:
       if (record->event.pressed) {
@@ -50,11 +100,6 @@ bool process_custom_macro_keys(uint16_t keycode, keyrecord_t *record) {
     case PRV_WD:
       if (record->event.pressed) {
         SEND_STRING(SS_LALT("a"));
-      }
-      break;
-    case PASSWD:
-      if (record->event.pressed) {
-        SEND_STRING("Test test test");
       }
       break;
   }
@@ -90,58 +135,96 @@ bool process_custom_macro_keys(uint16_t keycode, keyrecord_t *record) {
 */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[BASE] = LAYOUT_ergodox(
-                                                                    /* left hand */
-  /* top row */                             KC_ESC,   KC_1,    KC_2,     KC_3,    KC_4,    KC_5,    KC_6,
-                                            KC_TAB,   KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,
-  /* index, large top key */                                                                        KC_A,
-  /* home row */                            MO(1),    KC_A,    KC_S,     KC_D,    KC_F,    KC_G,
-                                            KC_LSFT,  KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,
-  /* index, large bottom key */                                                                     MO(1),
-  /* bottom row */                          KC_LCTRL, KC_LALT, KC_LCTRL, KC_LALT, KC_LGUI,
-  /* thumb, small top keys */                                                                       KC__VOLDOWN, KC__VOLUP,
-  /* thumb, small middle key */                                                                                  RESET,
-  /* thumb, big keys */                                                                  KC_SPC,    KC_BSPC,
-  /* thumb, small bottom key */                                                                                  MO(2),
 
-                                                  /* right hand */
-                         MO(2),      KC_7,    KC_8,    KC_9,    KC_0,     KC_LBRACKET, KC_RBRACKET, /* top row */
-                         KC_MINUS,                                                                  /* index, large top key */
-                                     KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,        KC_BSLS,     /* home row */
-                                     KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,     KC_QUOT,
-                         KC_EQUAL,                                                                  /* index, large bottom key */
-                                     KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,     KC_RSFT,
-                                              KC_LGUI, KC_RALT, KC_RCTRL, KC_RALT,     KC_RCTRL,    /* bottom row */
-        KC_MPLY, KC_BSPC,                                                                           /* thumb, small top keys */
-                  RESET,                                                                            /* thumb, small middle key */
-                  MO(2),                                                                            /* thumb, small bottom key */
-                         KC_ENT,    KC_SPC                                                          /* thumb, big keys */
+/************************/
+/* LAYER 0: BASIC LAYER */
+/************************/
+[BASE] = LAYOUT_ergodox(
+                                                           /* left hand */
+  /* top row */                   KC_ESC,   KC_1,    KC_2,     KC_3,    KC_4,      KC_5,    _______,
+                                  KC_TAB,   KC_Q,    KC_W,     KC_E,    KC_R,      KC_T,
+  /* index, large top key */                                                                MO(1),
+  /* home row */                  MO(1),    KC_A,    KC_S,     KC_D,    KC_F,      KC_G,
+                                  KC_LSFT,  KC_Z,    KC_X,     KC_C,    KC_V,      KC_B,
+  /* index, large bottom key */                                                             MO(2),
+  /* bottom row */                          KC_LPRN, KC_RPRN,  _______, KC_LCTRL,  KC_LALT,
+  /* thumb, small top keys */                                                               KC_HOME, KC_END,
+  /* thumb, small middle key */                                                                      KC_ENT,
+  /* thumb, big keys */                                                          KC_SPC,    KC_BSPC,
+  /* thumb, small bottom key */                                                                      KC_LGUI,
+
+                                                                            /* right hand */
+  /* top row */                                       _______, KC_6,    KC_7,    KC_8,     KC_9,        KC_0,        _______,
+  /* index, large top key */                          MO(1),
+                                                               KC_Y,    KC_U,    KC_I,     KC_O,        KC_P,        KC_BSLS,
+  /* home row */                                               KC_H,    KC_J,    KC_K,     KC_L,        KC_SCLN,     KC_QUOT,
+  /* index, large bottom key */                       MO(2),
+                                                               KC_N,    KC_M,    KC_COMM,  KC_DOT,      KC_SLSH,     KC_RSFT,
+  /* bottom row */                                                      KC_RALT, KC_RCTRL, _______,     KC_LBRACKET, KC_RBRACKET,
+  /* thumb, small top keys */               KC_MINUS, KC_PLUS,
+  /* thumb, small middle key */             KC_BSPC,
+  /* thumb, small bottom key */             KC_RGUI,
+  /* thumb, big keys */                               KC_ENT,  KC_SPC
 ),
-/* Keymap 1: Basic layer */
-[NAV] = LAYOUT_ergodox(
+
+/*****************************/
+/* LAYER 1: NAVIGATION LAYER */
+/*****************************/
+[LYR1] = LAYOUT_ergodox(
                                                         /* left hand */
   /* top row */                  KC_GRV,  _______, _______, _______, _______, _______, _______,
                                  KC_ENT,  _______, KC_UP,   _______, _______, _______,
   /* index, large top key */                                                           _______,
-  /* home row */                 _______, KC_LEFT, KC_DOWN, KC_RGHT, TMUX_CYC,_______,
-                                 _______, _______, _______, TMUX_C,  _______, _______,
+  /* home row */                 _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
+                                 _______, _______, _______, _______, _______, _______,
   /* index, large bottom key */                                                        _______,
   /* bottom row */               _______, _______, _______, _______, _______,
-  /* thumb, small top keys */                                                          KC_MRWD, KC_MFFD,
+  /* thumb, small top keys */                                                          _______, _______,
   /* thumb, small middle key */                                                                 _______,
   /* thumb, big keys */                                                       _______, KC_DEL,
   /* thumb, small bottom key */                                                                 _______,
-                                  /* right hand */
-            _______, _______, _______, _______, _______, _______, _______, /* top row */
-            _______,                                                       /* index, large top key */
-                     _______, _______, PRV_WD,  NXT_WD,  TMUX_P,  _______, /* home row */
-                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-            _______,                                                       /* index, large bottom key */
-                     TMUX_N,  _______, _______, _______, _______, _______,
-                              _______, _______, _______, _______, PASSWD,  /* bottom row */
-  _______,  KC_DEL,                                                        /* thumb, small top keys */
-  _______,                                                                 /* thumb, small middle key */
-  _______,                                                                 /* thumb, small bottom key */
-            _______, _______                                               /* thumb, big keys */
+
+                                                                           /* right hand */
+  /* top row */                                     _______, _______, _______, _______, _______, _______,     _______,
+  /* index, large top key */                        _______,
+                                                             TMUX_C,  TMUX_CYC,PRV_WD,  NXT_WD,  TMUX_P,      _______,
+  /* home row */                                             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,     _______,
+  /* index, large bottom key */                     _______,
+                                                             TMUX_N,  _______, _______, _______, _______,     _______,
+  /* bottom row */                                                    KC_MRWD, KC_MPLY, KC_MFFD, KC__VOLDOWN, KC__VOLUP,
+  /* thumb, small top keys */             _______,  _______,
+  /* thumb, small middle key */           _______,
+  /* thumb, small bottom key */           _______,
+  /* thumb, big keys */                             _______, _______
+),
+/*************************/
+/* LAYER 2: NUMPAD LAYER */
+/*************************/
+[LYR2] = LAYOUT_ergodox(
+                                                        /* left hand */
+  /* top row */                  _______, _______, _______, _______, _______, _______, _______,
+                                 _______, _______, _______, _______, _______, _______,
+  /* index, large top key */                                                           _______,
+  /* home row */                 _______, _______, _______, _______, _______, _______,
+                                 _______, _______, _______, _______, _______, _______,
+  /* index, large bottom key */                                                        _______,
+  /* bottom row */               P_CMP,   P_1PASS, P_OKTA,  P_CP,    P_LP,
+  /* thumb, small top keys */                                                          _______, _______,
+  /* thumb, small middle key */                                                                 _______,
+  /* thumb, big keys */                                                       _______, _______,
+  /* thumb, small bottom key */                                                                 _______,
+
+                                                                           /* right hand */
+  /* top row */                                     _______, _______, KC_ESC,  KC_PSLS, KC_PAST, KC_PMNS,  _______,
+  /* index, large top key */                        _______,
+                                                             _______, KC_7,    KC_8,    KC_9,    KC_PPLS,  _______,
+  /* home row */                                             _______, KC_4,    KC_5,    KC_6,    KC_PPLS,  _______,
+  /* index, large bottom key */                     _______,
+                                                             _______, KC_1,    KC_2,    KC_3,    KC_PENT,  _______,
+  /* bottom row */                                                    KC_0,    KC_PDOT, KC_PENT, KC_PENT,  _______,
+  /* thumb, small top keys */             _______,  _______,
+  /* thumb, small middle key */           _______,
+  /* thumb, small bottom key */           _______,
+  /* thumb, big keys */                             _______, _______
 ),
 };
