3528cd46f34b736556a744f70c2126bff765ad41
Tom Craig <thetomcraig@icloud.com>
2019-09-08 17:04:58 -0700

    Move all stuff to common

--- common_macros.h
+++ common_macros.h
@@ -5,7 +5,25 @@ extern keymap_config_t keymap_config;
 
 
 enum custom_keycodes {
-  TMUX_N=SAFE_RANGE,
+  // iris
+  BASE = SAFE_RANGE,
+  LYR1,
+  LYR2,
+  LYR3,
+  // ergodox
+  EPRM,
+  VRSN,
+  RGB_SLD,
+
+  // passwords
+  P_CMP,
+  P_1PASS,
+  P_OKTA,
+  P_CP,
+  P_LP,
+
+  // tmux
+  TMUX_N,
   TMUX_P,
   TMUX_C,
   TMUX_CYC,
@@ -20,17 +38,10 @@ enum custom_keycodes {
   TMUX_7,
   TMUX_8,
   TMUX_9,
+
   PRV_WD,
   NXT_WD,
-  NP_COL,
-  P_CMP,
-  P_1PASS,
-  P_OKTA,
-  P_CP,
-  P_LP,
 };
-
-
 
 
 // This is only for passwords
@@ -72,98 +83,104 @@ bool process_password_keys(uint16_t keycode, keyrecord_t *record) {
 }
 
 // This is for my custom macros; keyboard agnostic
-bool process_custom_macros_keys(uint16_t keycode, keyrecord_t *record) {
-  case TMUX_N:
-    if (record->event.pressed) {
-      send_string(SS_LCTRL("b")"n");
-    }
-    break;
-  case TMUX_P:
-    if (record->event.pressed) {
-      send_string(SS_LCTRL("b")"p");
-    }
-    break;
-  case TMUX_C:
-    if (record->event.pressed) {
-      send_string(SS_LCTRL("b")"c");
-    }
-    break;
-  case TMUX_CYC:
-    if (record->event.pressed) {
-      send_string(SS_LCTRL("f"));
-    }
-    break;
-  case TMUX_SWAP_L:
-    if (record->event.pressed) {
-      send_string(SS_LCTRL("["));
-    }
-    break;
-  case TMUX_SWAP_R:
-    if (record->event.pressed) {
-      send_string(SS_LCTRL("]"));
-    }
-    break;
-  case TMUX_1:
-    if (record->event.pressed) {
-      send_string(SS_LCTRL("1"));
-    }
-    break;
-  case TMUX_2:
-    if (record->event.pressed) {
-      send_string(SS_LCTRL("2"));
-    }
-    break;
-  case TMUX_3:
-    if (record->event.pressed) {
-      send_string(SS_LCTRL("3"));
-    }
-    break;
-  case TMUX_4:
-    if (record->event.pressed) {
-      send_string(SS_LCTRL("4"));
-    }
-    break;
-  case TMUX_5:
-    if (record->event.pressed) {
-      send_string(SS_LCTRL("5"));
-    }
-    break;
-  case TMUX_6:
-    if (record->event.pressed) {
-      send_string(SS_LCTRL("6"));
-    }
-    break;
-  case TMUX_7:
-    if (record->event.pressed) {
-      send_string(SS_LCTRL("7"));
-    }
-    break;
-  case TMUX_8:
-    if (record->event.pressed) {
-      send_string(SS_LCTRL("8"));
-    }
-    break;
-  case TMUX_9:
-    if (record->event.pressed) {
-      send_string(SS_LCTRL("9"));
-    }
-    break;
-  case NXT_WD:
-    if (record->event.pressed) {
-      send_string(SS_DOWN(X_LALT) SS_DOWN(X_RIGHT));
-    }
-    else {
-      send_string(SS_UP(X_RIGHT) SS_UP(X_LALT));
-    }
-    break;
-  case PRV_WD:
-    if (record->event.pressed) {
-      send_string(SS_DOWN(X_LALT) SS_DOWN(X_LEFT));
-    }
-    else {
-      send_string(SS_UP(X_LEFT) SS_UP(X_LALT));
-    }
-    break;
+bool process_custom_macro_keys(uint16_t keycode, keyrecord_t *record) {
+  bool still_need_to_process = process_password_keys(keycode, record);
+  if (still_need_to_process == false) {
+    return false;
   }
+
+  switch (keycode) {
+    case TMUX_N:
+      if (record->event.pressed) {
+        send_string(SS_LCTRL("b")"n");
+      }
+      break;
+    case TMUX_P:
+      if (record->event.pressed) {
+        send_string(SS_LCTRL("b")"p");
+      }
+      break;
+    case TMUX_C:
+      if (record->event.pressed) {
+        send_string(SS_LCTRL("b")"c");
+      }
+      break;
+    case TMUX_CYC:
+      if (record->event.pressed) {
+        send_string(SS_LCTRL("f"));
+      }
+      break;
+    case TMUX_SWAP_L:
+      if (record->event.pressed) {
+        send_string(SS_LCTRL("["));
+      }
+      break;
+    case TMUX_SWAP_R:
+      if (record->event.pressed) {
+        send_string(SS_LCTRL("]"));
+      }
+      break;
+    case TMUX_1:
+      if (record->event.pressed) {
+        send_string(SS_LCTRL("1"));
+      }
+      break;
+    case TMUX_2:
+      if (record->event.pressed) {
+        send_string(SS_LCTRL("2"));
+      }
+      break;
+    case TMUX_3:
+      if (record->event.pressed) {
+        send_string(SS_LCTRL("3"));
+      }
+      break;
+    case TMUX_4:
+      if (record->event.pressed) {
+        send_string(SS_LCTRL("4"));
+      }
+      break;
+    case TMUX_5:
+      if (record->event.pressed) {
+        send_string(SS_LCTRL("5"));
+      }
+      break;
+    case TMUX_6:
+      if (record->event.pressed) {
+        send_string(SS_LCTRL("6"));
+      }
+      break;
+    case TMUX_7:
+      if (record->event.pressed) {
+        send_string(SS_LCTRL("7"));
+      }
+      break;
+    case TMUX_8:
+      if (record->event.pressed) {
+        send_string(SS_LCTRL("8"));
+      }
+      break;
+    case TMUX_9:
+      if (record->event.pressed) {
+        send_string(SS_LCTRL("9"));
+      }
+      break;
+    case NXT_WD:
+      if (record->event.pressed) {
+        send_string(SS_DOWN(X_LALT) SS_DOWN(X_RIGHT));
+      }
+      else {
+        send_string(SS_UP(X_RIGHT) SS_UP(X_LALT));
+      }
+      break;
+    case PRV_WD:
+      if (record->event.pressed) {
+        send_string(SS_DOWN(X_LALT) SS_DOWN(X_LEFT));
+      }
+      else {
+        send_string(SS_UP(X_LEFT) SS_UP(X_LALT));
+      }
+      break;
+    }
   return true;
 }
