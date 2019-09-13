8a794640ef201eecf5fe44378b563dd8b7c44443
Tom Craig <thetomcraig@icloud.com>
2019-09-13 14:57:10 -0700

    upadte teensy instruction

--- flash_ergodox.sh
+++ flash_ergodox.sh
@@ -1,9 +1,6 @@
 set -e
-# Setup left
-echo "Get sudo password saved"
-# sudo ls
-
 echo "PUT KB INTO FLASH MODE"
 make ergodox_ez:${1}
 # sudo make ergodox_ez:${1}:teensy
 # ./teensy_loader_cli -w --mcu atmega32u4 ergodox_ez_${1}.hex
+echo "OPEN TEENSY GUI TO FLASH"
